242ce15343162d1a887b9e402244a43b946d0710
Chris Lloyd <cjl8zf@virginia.edu>
2021-02-15 20:03:10 -0500

    Initial commit

--- README.md
+++ README.md
@@ -2,7 +2,38 @@
 
 ## Summary
 
-Force langtool to ignore certain fonts. For example don't highlight LaTeX in math-mode.
+Force [langtool](https://github.com/languagetool-org/languagetool) to
+ignore certain fonts. For example don't highlight LaTeX in math-mode.
+
+When running [Emacs-Langtool](https://github.com/mhayashi1120/Emacs-langtool) on a
+buffer containing a markup language Langtool will highlight many false
+positives. This is especially true for documents written in LaTeX.
+
+This package solves this problem by allowing one to leverage the power
+of their given major-mode's syntax highlighting to ignore a certain
+font. The idea here is that the major mode should have put in a lot of
+thought to identifying the various parts of the document.
+
+For instance by telling Langtool to ignore 'font-latex-math-face
+Langtool will no longer complain about math expressions. This was my
+main use case while writing my thesis. This effectively adds langtool
+support for LaTeX in Emacs. One simpliy adds a hook for their desired
+mode and sets the list `'langtool-ignore-fonts` to the desired list of
+ignored fonts. We provide out of the box support for LaTeX as follows:
+
+```
+(add-hook 'latex-mode-hook (lambda () 
+	(setq langtool-ignore-fonts '(font-lock-comment-face font-latex-math-face))))
+```
+
+However, I can see this being used for many other major-modes. For
+instance to set markdown-mode to ignore code comments add the
+following to your init file after loading this package.
+
+```
+(add-hook 'markdown-mode-hook (lambda () 
+	(setq langtool-ignore-fonts '(markdown-code-face))))
+```
 
 ## Installing
 
@@ -12,6 +43,25 @@ build the project.
     cd langtool-ignore-fonts
     make && make install
 
+This should install the package to your elpa folder. Then import:
+
+```
+(require 'langtool-ignore-fonts "~/.emacs.d/elpa/langtool-ignore-fonts")
+```
+
+Or you can use use-package (once you have installed it locally): 
+```
+(use-package langtool-ignore-fonts
+  :load-path "~/.emacs.d/elpa/")
+```
+You could add your markdown font list here for instance 
+```
+(use-package langtool-ignore-fonts
+  :load-path "~/.emacs.d/elpa/"
+  :config 
+  (add-hook 'markdown-mode-hook (lambda () 
+	(setq langtool-ignore-fonts '(markdown-code-face)))))
+```
 ## Contributing
 
 Yes, please do! See [CONTRIBUTING][] for guidelines.
--- langtool-ignore-fonts.el
+++ langtool-ignore-fonts.el
@@ -1,8 +1,8 @@
-;;; langtool-ignore-fonts.el --- Force langtool to ignore certain fonts. For example don't highlight LaTeX in math-mode.
+;;; langtool-ignore-fonts.el --- Force langtool to ignore certain fonts.
 
 ;; Copyright (C) 2021 Chrstopher Lloyd
 
-;; Author: Chrstopher Lloyd <chris@d-172-26-121-103.dhcp.virginia.edu>
+;; Author: Chrstopher Lloyd <cjl8zf@virginia.edu>
 ;; Version: 0.1
 
 ;; This file is not part of GNU Emacs.
@@ -22,11 +22,75 @@
 
 ;;; Commentary:
 
-;; Force langtool to ignore certain fonts. For example don't highlight LaTeX in math-mode.
+;; Force langtool to ignore certain fonts.
 
 ;;; Code:
+(defcustom langtool-ignore-fonts nil;; '(font-lock-comment-face font-latex-math-face)
+  "List of font faces that langtool should ignore.
+
+   For example, the default values prevent langtool from trying to
+   correct LaTeX math or comments.
+
+   This variable is buffer local so that the behavior of langtool
+   can be altered based on the current 'major-mode."
+  :type 'sexp
+  :group 'langtool)
+
+(make-local-variable 'langtool-ignore-fonts)
+
+(defun get-langtool-overlays ()
+  "Find all of the langtool overlays.
+
+  This function assumes that langtool-check has already been run.
+
+  We first call 'font-lock-ensure to ensure that reigons outside
+  of acessible buffer have had font-lock applied."
+  (progn
+    (font-lock-ensure)
+    (seq-filter
+     (lambda (ov) (overlay-get ov 'langtool-message))
+     (overlays-in 0 (buffer-size)))))
+
+(defun overlay-has-matched-font (ov)
+  "Check to see if the overlay OV region font is on our list of fonts."
+  (or (matched-font-at (overlay-start ov)) (matched-font-at (overlay-end ov))))
 
 
+(defun matched-font-at (pos)
+  "Check to see if the character at POS has a font from the langtool-ignore-fonts list."
+  (< 0 (length (cl-intersection langtool-ignore-fonts
+				(treat-as-list (get-text-property pos 'face))))))
+
+(defun treat-as-list (x)
+  "If X is a single value wrap it as a singleton otherwise leave it alone."
+  (if (listp x) x (list x)))
+
+(defun get-langtool-matched-font-overlays ()
+  "Get a list of all of the langtool-overlays that match the langtool-ignore-fonts list."
+  (seq-filter #'overlay-has-matched-font (get-langtool-overlays)))
+
+
+(defun delete-langtool-matched-font-overlays ()
+  "Delete any langtool overlays that match the font list langtool-ignore-fonts."
+  (interactive)
+  (mapc #'delete-overlay (get-langtool-matched-font-overlays)))
+
+(defun delete-langtool-matched-font-overlays-advice (&rest args)
+  "Advise with ARGS to remove langtool overlays that match the font list.
+   
+  This function should be called after 'langtool--check-finish."
+  (progn
+    (message "Removing langtool overlays matching 'langtool-ignore-fonts.")
+    (mapc #'delete-overlay (get-langtool-matched-font-overlays))
+    (message "Done removing overlays.")))
+
+
+;;;  Add support for LaTeX
+(add-hook 'LaTeX-mode-hook (lambda ()
+			     (setq langtool-ignore-fonts '(font-lock-comment-face
+							   font-latex-math-face))))
+
+(advice-add 'langtool--check-finish :after #'delete-langtool-matched-font-overlays-advice)
 
 (provide 'langtool-ignore-fonts)
 
